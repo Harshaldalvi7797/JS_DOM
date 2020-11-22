2d7bcaa4b0a91f6fa331a5051d63dede1c9434d7
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-11-23 04:40:49 +0530

    commit: getelementbyid

--- dom.js
+++ dom.js
@@ -1,3 +1,29 @@
 /** @format */
+
 //Examine the document object
 //console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all[0]);
+// document.all[10].textContent = "Hello";
+// console.log(document.forms);
+// console.log(document.links)
+//console.log(document.images);
+
+//GET ELEMENTS BY ID
+console.log(document.getElementById("header-title"));
+var headerTitle = document.getElementById("header-title");
+console.log(headerTitle);
+
+headerTitle.textContent = "Hello";
+hederTitle.innerText = "Good Bye";
+
+// var items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[1]);
