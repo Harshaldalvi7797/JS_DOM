4a2d39293e11010024f32ecc12208b0e2c09d15c
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-11-26 20:13:51 +0530

    commit : using class name

--- dom.js
+++ dom.js
@@ -17,13 +17,28 @@
 //console.log(document.images);
 
 //GET ELEMENTS BY ID
-console.log(document.getElementById("header-title"));
-var headerTitle = document.getElementById("header-title");
-console.log(headerTitle);
+// console.log(document.getElementById("header-title"));
+// var headerTitle = document.getElementById("header-title");
+// var nav = document.getElementById("navbar");
+// console.log(headerTitle);
 
-headerTitle.textContent = "Hello";
-hederTitle.innerText = "Good Bye";
+// headerTitle.textContent = "Hello";
+// headerTitle.innerText = "<h3>Good Bye</h3>";
 
 // var items = document.getElementsByClassName("list-group-item");
 // console.log(items);
-// console.log(items[1]);
+// console.log(items[0]);
+// headerTitle.style.borderBottom = "solid 3px #000";
+// nav.style.borderBottom = "solid 30px #000";
+
+
+//class
+var con = document.getElementsByClassName("list-group-item");
+console.log(con);
+console.log(con[1]);
+con[1].textContent = "hello jjj";
+con[1].style.fontWeight = "bold";
+con[1].style.backgroundColor = "yellow";
+for (var i = 0; i <= con.length; i++) {
+  con[i].style.backgroundColor = "#f4f4f4";
+}
